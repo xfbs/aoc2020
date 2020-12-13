3a6840997070f225aaf5da9ffeb75bb6e3a6e023
Patrick Elsen <pelsen@xfbs.net>
2020-12-13 22:26:54 +0100

    adds solution for part 2

--- 13-shuttle-search/src/main.rs
+++ 13-shuttle-search/src/main.rs
@@ -7,7 +7,11 @@ fn main() {
 
     for line in lines {
         let line = line.unwrap();
-        let busses: Vec<_> = line.split(",").filter(|x| *x != "x").map(|x| x.parse::<usize>().unwrap()).collect();
+        let busses: Vec<_> = line
+            .split(",")
+            .filter(|x| *x != "x")
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect();
         let mut departures: Vec<(usize, usize)> = busses
             .iter()
             .map(|b| match timestamp % b {
@@ -19,5 +23,34 @@ fn main() {
         let departure = departures.first().unwrap();
         println!("departure {:?}", departure);
         println!("result {:?}", departure.0 * departure.1);
+
+        // second part
+        let busses: Vec<Option<usize>> = line
+            .split(",")
+            .map(|x| match x {
+                "x" => None,
+                _ => Some(x.parse::<usize>().unwrap()),
+            })
+            .collect();
+
+        let mut offset = 0;
+        let mut product = busses[0].unwrap();
+
+        for (i, bus) in busses.iter().enumerate().skip(1) {
+            if let Some(bus) = bus {
+                let mut i = i;
+                while i > *bus {
+                    i -= bus;
+                }
+
+                while offset % bus != bus - i {
+                    offset += product;
+                }
+
+                product *= bus;
+            }
+        }
+
+        println!("offset {}", offset);
     }
 }
